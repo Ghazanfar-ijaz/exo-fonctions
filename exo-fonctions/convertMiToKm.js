395f282929a321669e9a690e0f1f10632d628892
Ghazanfar-ijaz <78793183+Ghazanfar-ijaz@users.noreply.github.com>
2021-03-04 21:11:26 +0100

    Add files via upload

---
+++ exo-fonctions/convertMiToKm.js
@@ -0,0 +1,7 @@
+// Une fonction  qui fait la conversion d'une distance exprimée en Milles, en Kilomètres:
+
+const convertMiToKm = (MiToMk) => {
+  let Km = 1.60934
+  return distance = MiToMk * Km
+}
+console.log(convertMiToKm(5))
